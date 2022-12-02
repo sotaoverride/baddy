d20584c94041c45ea591d9066d00490649ac588b
Aatir Manzur <aatrapps@gmail.com>
2022-12-02 13:25:18 -0500

    i2c simple rw

--- camera.c
+++ camera.c
@@ -27,5 +27,15 @@ int main() {
     gpio_set_function(25, GPIO_FUNC_I2C);
     gpio_pull_up(24);
     gpio_pull_up(25);
+    uint8_t txdata = 0xAA;
+    uint8_t rxdata;
+    i2c_write_blocking(i2c_default, 0x00, &txdata, 1, false);
+    i2c_write_blocking(i2c_default, 0x01, &txdata, 1, false);
+    i2c_write_blocking(i2c_default, 0x02, &txdata, 1, false);
+    i2c_write_blocking(i2c_default, 0x03, &txdata, 1, false);
+    i2c_read_blocking(i2c_default, &rxdata, 0x00, 1 , false);
+    i2c_read_blocking(i2c_default, &rxdata, 0x01, 1 , false);
+    i2c_read_blocking(i2c_default, &rxdata, 0x02, 1 , false);
+    i2c_read_blocking(i2c_default, &rxdata, 0x03, 1 , false);
     while(1) {}
 }
