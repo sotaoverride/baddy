3d3a1a1d125dc4fdd54fdc270964a6b481e92ed3
Aatir Manzur <aatrapps@gmail.com>
2022-12-26 16:50:44 -0500

    scan i2c bus for devices

--- camera.c
+++ camera.c
@@ -7,40 +7,53 @@
 #include "hardware/clocks.h"
 
 
+// I2C reserves some addresses for special purposes. We exclude these from the scan.
+// These are any addresses of the form 000 0xxx or 111 1xxx
+bool reserved_addr(uint8_t addr) {
+    return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
+}
 
 int main() {
     stdio_init_all();
-
-
-    gpio_init(10);
-    gpio_init(7);
-    gpio_init(6);
-    gpio_init(5);
-    gpio_init(4);
-    gpio_init(11);
-    gpio_init(2);
-    gpio_init(9);
-    clock_gpio_init(27, CLOCKS_CLK_GPOUT0_CTRL_AUXSRC_VALUE_CLK_SYS, 10);
-
-    //use pin 22, 21 for I2c0 scl0, sda0
+    printf("camera prg starting************");
+    clocks_init();
+    gpio_init(22);
+    clock_gpio_init(22, CLOCKS_CLK_GPOUT0_CTRL_AUXSRC_VALUE_CLK_SYS, 10);
     i2c_init(i2c_default, 100 * 1000);
-    gpio_set_function(21, GPIO_FUNC_I2C);
-    gpio_set_function(22, GPIO_FUNC_I2C);
-    gpio_pull_up(21);
-    gpio_pull_up(22);
-    uint8_t txdata = 0xAA;
-    uint8_t rxdata;
-    i2c_write_blocking(i2c_default, 0x00, &txdata, 1, false);
-    i2c_write_blocking(i2c_default, 0x01, &txdata, 1, false);
-    i2c_write_blocking(i2c_default, 0x02, &txdata, 1, false);
-    i2c_write_blocking(i2c_default, 0x03, &txdata, 1, false);
-    i2c_read_blocking(i2c_default, 0x00, &rxdata, 1 , false);
-    printf("value ad 0x00 0xAA=%u ?\n", rxdata);
-    i2c_read_blocking(i2c_default, 0x01, &rxdata, 1, false);
-    i2c_read_blocking(i2c_default, 0x02, &rxdata, 1, false);
-    i2c_read_blocking(i2c_default, 0x03, &rxdata, 1, false);
-    while(1) {
-    	printf("value at 0x00 0xAA=%u ?\n", rxdata);
-    	
+    gpio_set_function(16, GPIO_FUNC_I2C);
+    gpio_set_function(17, GPIO_FUNC_I2C);
+    gpio_pull_up(16);
+    gpio_pull_up(17);
+
+    printf("\nI2C Bus Scan\n");
+    printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
+
+    while(1){
+    printf("\nI2C Bus Scan\n");
+    printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
+	    
+    for (int addr = 0; addr < (1 << 7); ++addr) {
+        if (addr % 16 == 0) {
+            printf("%02x ", addr);
+        }
+
+        // Perform a 1-byte dummy read from the probe address. If a slave
+        // acknowledges this address, the function returns the number of bytes
+        // transferred. If the address byte is ignored, the function returns
+        // -1.
+
+        // Skip over any reserved addresses.
+        int ret;
+        uint8_t rxdata;
+        if (reserved_addr(addr))
+            ret = -1;
+        else
+            ret = i2c_read_blocking(i2c_default, addr, &rxdata, 1, false);
+
+        printf(ret < 0 ? "." : "@");
+        printf(addr % 16 == 15 ? "\n" : "  ");
+    }
+    printf("Done. \n");
     }
 }
+
