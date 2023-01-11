da800094bbcc5a57217c7bb9dd6ba60a43624e45
Aatir Manzur <aatrapps@gmail.com>
2023-01-11 15:31:07 -0500

    add vendor ID read

--- camera.c
+++ camera.c
@@ -27,24 +27,50 @@ int main() {
     gpio_pull_up(4);
     gpio_pull_up(5);
 
-    printf("\nI2C Bus Scan\n");
-    printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
+    //data lines
+    //
+    gpio_init(0);
+    gpio_set_dir(0, GPIO_IN);
+    gpio_init(1);
+    gpio_set_dir(1, GPIO_IN);
+    gpio_init(2);
+    gpio_set_dir(2, GPIO_IN);
+    gpio_init(3);
+    gpio_set_dir(3, GPIO_IN);
+    gpio_init(8);
+    gpio_set_dir(8, GPIO_IN);
+    gpio_init(9);
+    gpio_set_dir(9, GPIO_IN);
+    gpio_init(10);
+    gpio_set_dir(10, GPIO_IN);
+    gpio_init(11);
+    gpio_set_dir(11, GPIO_IN);
+
+    //test read
+    //
+    const uint8_t regaddr[1] = { 0x1c };
+    i2c_write_blocking(i2c_default, 0x21, &regaddr, 1, true);
+    uint8_t val[1];
+    i2c_read_blocking(i2c_default, 0x21, &val, 1, false);
+
+    printf("\ni2c bus scan\n");
+    printf("   0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\n");
 
     while(1){
-    printf("\nI2C Bus Scan\n");
-    printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
+    printf("\ni2c bus scan\n");
+    printf("   0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\n");
 	    
     for (int addr = 0; addr < (1 << 7); ++addr) {
         if (addr % 16 == 0) {
             printf("%02x ", addr);
         }
 
-        // Perform a 1-byte dummy read from the probe address. If a slave
+        // perform a 1-byte dummy read from the probe address. if a slave
         // acknowledges this address, the function returns the number of bytes
-        // transferred. If the address byte is ignored, the function returns
+        // transferred. if the address byte is ignored, the function returns
         // -1.
 
-        // Skip over any reserved addresses.
+        // skip over any reserved addresses.
         int ret;
         uint8_t rxdata;
         if (reserved_addr(addr))
@@ -56,6 +82,7 @@ int main() {
         printf(addr % 16 == 15 ? "\n" : "  ");
     }
     printf("Done. \n");
+    printf("Vendor ID: %02x", val[0]);
     }
 
 }
